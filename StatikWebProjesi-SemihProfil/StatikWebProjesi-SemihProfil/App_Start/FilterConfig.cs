64e2c7e97a007cbad493cdc21785961cd1cb05b0
SemihVRL <143089186+SemihVRL@users.noreply.github.com>
2024-03-22 12:11:22 +0300

    Mvc-Profile

---
+++ StatikWebProjesi-SemihProfil/StatikWebProjesi-SemihProfil/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace StatikWebProjesi_SemihProfil
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ StatikWebProjesi-SemihProfil/StatikWebProjesi-SemihProfil/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StatikWebProjesi_SemihProfil.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Profil()
+        {
+            return View();
+        }
+    }
+}
